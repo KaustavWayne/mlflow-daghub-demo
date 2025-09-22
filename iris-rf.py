a027d4e7e7e6e74077801ab8998906b1e59e639c
KaustavWayne <kaustavking2@gmail.com>
2025-09-22 21:17:03 +0530

    add rf experiment

--- iris-rf.py
+++ iris-rf.py
@@ -1,12 +1,13 @@
 import mlflow
 import mlflow.sklearn
 from sklearn.datasets import load_iris
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
 import dagshub
+import shutil  # ✅ import shutil
 import os
 
 # Initialize DagsHub integration
@@ -21,21 +22,24 @@ y = iris.target
 # Train-test split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-max_depth = 16
+max_depth = 1
+n_estimators = 100
 
-mlflow.set_experiment('iris-dt')
+mlflow.set_experiment('iris-rf')
 
 with mlflow.start_run() as run:
 
-    dt = DecisionTreeClassifier(max_depth=max_depth)
-    dt.fit(X_train, y_train)
-    y_pred = dt.predict(X_test)
+    rf = RandomForestClassifier(max_depth=max_depth, n_estimators=n_estimators)
+    rf.fit(X_train, y_train)
+    y_pred = rf.predict(X_test)
 
     accuracy = accuracy_score(y_test, y_pred)
 
     # Log metrics & params
     mlflow.log_metric('accuracy', accuracy)
+
     mlflow.log_param('max_depth', max_depth)
+    mlflow.log_param('n_estimators', n_estimators)
 
     # Confusion Matrix plot
     cm = confusion_matrix(y_test, y_pred)
@@ -51,23 +55,19 @@ with mlflow.start_run() as run:
     mlflow.log_artifact("confusion_matrix.png")
     mlflow.log_artifact(__file__)
 
-    # You can use manually by renaming it
-
-    #model_path = "decision_tree_model"
-    #mlflow.sklearn.save_model(sk_model=dt, path=model_path)
-    #mlflow.log_artifact(model_path)
-
-    # ✅ Save and log model with unique folder name
+    # ✅ Save model with unique run_id folder
     run_id = run.info.run_id
-    model_path = f"decision_tree_model_{run_id}"
+    model_path = f"random_forest_model_{run_id}"
 
-    mlflow.sklearn.save_model(sk_model=dt, path=model_path)
+    mlflow.sklearn.save_model(sk_model=rf, path=model_path)
     mlflow.log_artifact(model_path)
 
+    # Delete the local folder after logging
+    shutil.rmtree(model_path)  # ✅ removes folder from VSCode project
 
     # Tags
-    mlflow.set_tag('author', 'kaustav')
-    mlflow.set_tag('model', 'decision tree')
+    mlflow.set_tag('author', 'priyanka')
+    mlflow.set_tag('model', 'random_forest')
 
     print('accuracy', accuracy)
-    print(f"Model saved at: {model_path}")
+    print(f"Model logged and local folder '{model_path}' deleted.")
